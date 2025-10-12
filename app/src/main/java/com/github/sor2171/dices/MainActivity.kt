c24f182c90365ca0401c48b33481c37cd196bb8b
SOR <3272194354@qq.com>
2025-10-12 10:01:01 +0800

    theme and button

--- app/src/main/java/com/github/sor2171/dices/MainActivity.kt
+++ app/src/main/java/com/github/sor2171/dices/MainActivity.kt
@@ -1,14 +1,21 @@
 package com.github.sor2171.dices
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.github.sor2171.dices.ui.theme.DicesTheme
+import androidx.compose.ui.unit.dp
+import com.github.sor2171.dices.ui.component.TextButton
+import com.github.sor2171.dices.ui.theme.AppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,17 +29,37 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MainFunc() {
-    DicesTheme {
-        Scaffold(
+    AppTheme(
+        dynamicColor = false
+    ) {
+        Surface(tonalElevation = 5.dp) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                TextButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(vertical = 12.dp),
+                    textID = R.string.SIButton_name
+                )
 
-        ) { paddingValues ->
-            Text("")
-            val p = paddingValues
+                TextButton(
+                    modifier = Modifier
+                        .fillMaxWidth(),
+                    textID = R.string.MDButton_name
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    name = "DefaultPreviewLight"
+)
 @Composable
 fun MainActivityPreview() {
     MainFunc()
---
+++ app/src/main/java/com/github/sor2171/dices/ui/component/TextButton.kt
@@ -0,0 +1,23 @@
+package com.github.sor2171.dices.ui.component
+
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+
+@Composable
+fun TextButton(
+    modifier: Modifier,
+    textID: Int
+) {
+    Button(
+        onClick = {},
+        modifier = modifier
+    ) {
+        Text(
+            text = stringResource(id = textID)
+        )
+    }
+}
